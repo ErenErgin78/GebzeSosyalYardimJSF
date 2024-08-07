90c4b4bfc4e9a2de657891426873ffe0d79dcaf6
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-08-07 12:12:59 +0300

    küçük değişiklik

--- src/java/dao/MahalleDAO.java
+++ src/java/dao/MahalleDAO.java
@@ -6,8 +6,6 @@ package dao;
 
 import Entity.Mahalle;
 import static Various.ErrorFinder.DetectError;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
