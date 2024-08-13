361da1f68142ef4dc244b040558a63a89fbcb61b
Eren Ergin <115938507+ErenErgin78@users.noreply.github.com>
2024-08-13 14:43:14 +0300

    Muracaat eklendi

--- src/java/Entity/Muracaat.java
+++ src/java/Entity/Muracaat.java
@@ -1,5 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Entity;
 
+import java.math.BigInteger;
 import java.util.Date;
 
 /**
@@ -8,22 +13,59 @@ import java.util.Date;
  */
 public class Muracaat {
 
+    private Integer muracaat_id;
+    private Integer kisi_temel_id;
     private Integer muracaat_bilgi_id;
-    private Integer arsiv_dosya_no;
-    private String aciklama;
-    private Date muracaat_tarihi;
+    private Date kayit_tarihi;
     private Integer aktif;
-    private Date kayit_tarih;
-    private Date guncelleme_tarih;
+    private Date guncelleme_tarihi;
+
+    private BigInteger kimlik_no;
+    private String kisi_isim;
+    private String kisi_soyisim;
+
+    private Integer arsiv_dosya_no;
+    private Date muracaat_tarihi;
+
+    private String mahalle;
+    private String sokak;
+
+    public Muracaat(Integer kisi_temel_id, Integer muracaat_bilgi_id, Integer aktif) {
+        this.kisi_temel_id = kisi_temel_id;
+        this.muracaat_bilgi_id = muracaat_bilgi_id;
+        this.aktif = aktif;
+    }
+
+    public Muracaat(Date kayit_tarihi, Integer aktif, Date guncelleme_tarihi, BigInteger kimlik_no, String kisi_isim, String kisi_soyisim, Integer arsiv_dosya_no, Date muracaat_tarihi, String mahalle, String sokak) {
+        this.kayit_tarihi = kayit_tarihi;
+        this.aktif = aktif;
+        this.guncelleme_tarihi = guncelleme_tarihi;
+        this.kimlik_no = kimlik_no;
+        this.kisi_isim = kisi_isim;
+        this.kisi_soyisim = kisi_soyisim;
+        this.arsiv_dosya_no = arsiv_dosya_no;
+        this.muracaat_tarihi = muracaat_tarihi;
+        this.mahalle = mahalle;
+        this.sokak = sokak;
+    }
 
     public Muracaat() {
     }
 
-    public Muracaat(Integer arsiv_dosya_no, String aciklama, Date muracaat_tarihi, Integer aktif) {
-        this.arsiv_dosya_no = arsiv_dosya_no;
-        this.aciklama = aciklama;
-        this.muracaat_tarihi = muracaat_tarihi;
-        this.aktif = aktif;
+    public Integer getMuracaat_id() {
+        return muracaat_id;
+    }
+
+    public void setMuracaat_id(Integer muracaat_id) {
+        this.muracaat_id = muracaat_id;
+    }
+
+    public Integer getKisi_temel_id() {
+        return kisi_temel_id;
+    }
+
+    public void setKisi_temel_id(Integer kisi_temel_id) {
+        this.kisi_temel_id = kisi_temel_id;
     }
 
     public Integer getMuracaat_bilgi_id() {
@@ -34,28 +76,12 @@ public class Muracaat {
         this.muracaat_bilgi_id = muracaat_bilgi_id;
     }
 
-    public Integer getArsiv_dosya_no() {
-        return arsiv_dosya_no;
+    public Date getKayit_tarihi() {
+        return kayit_tarihi;
     }
 
-    public void setArsiv_dosya_no(Integer arsiv_dosya_no) {
-        this.arsiv_dosya_no = arsiv_dosya_no;
-    }
-
-    public String getAciklama() {
-        return aciklama;
-    }
-
-    public void setAciklama(String aciklama) {
-        this.aciklama = aciklama;
-    }
-
-    public Date getMuracaat_tarihi() {
-        return muracaat_tarihi;
-    }
-
-    public void setMuracaat_tarihi(Date muracaat_tarihi) {
-        this.muracaat_tarihi = muracaat_tarihi;
+    public void setKayit_tarihi(Date kayit_tarihi) {
+        this.kayit_tarihi = kayit_tarihi;
     }
 
     public Integer getAktif() {
@@ -66,19 +92,68 @@ public class Muracaat {
         this.aktif = aktif;
     }
 
-    public Date getKayit_tarih() {
-        return kayit_tarih;
+    public Date getGuncelleme_tarihi() {
+        return guncelleme_tarihi;
     }
 
-    public void setKayit_tarih(Date kayit_tarih) {
-        this.kayit_tarih = kayit_tarih;
+    public void setGuncelleme_tarihi(Date guncelleme_tarihi) {
+        this.guncelleme_tarihi = guncelleme_tarihi;
     }
 
-    public Date getGuncelleme_tarih() {
-        return guncelleme_tarih;
+    public BigInteger getKimlik_no() {
+        return kimlik_no;
     }
 
-    public void setGuncelleme_tarih(Date guncelleme_tarih) {
-        this.guncelleme_tarih = guncelleme_tarih;
+    public void setKimlik_no(BigInteger kimlik_no) {
+        this.kimlik_no = kimlik_no;
     }
+
+    public String getKisi_isim() {
+        return kisi_isim;
+    }
+
+    public void setKisi_isim(String kisi_isim) {
+        this.kisi_isim = kisi_isim;
+    }
+
+    public String getKisi_soyisim() {
+        return kisi_soyisim;
+    }
+
+    public void setKisi_soyisim(String kisi_soyisim) {
+        this.kisi_soyisim = kisi_soyisim;
+    }
+
+    public Integer getArsiv_dosya_no() {
+        return arsiv_dosya_no;
+    }
+
+    public void setArsiv_dosya_no(Integer arsiv_dosya_no) {
+        this.arsiv_dosya_no = arsiv_dosya_no;
+    }
+
+    public Date getMuracaat_tarihi() {
+        return muracaat_tarihi;
+    }
+
+    public void setMuracaat_tarihi(Date muracaat_tarihi) {
+        this.muracaat_tarihi = muracaat_tarihi;
+    }
+
+    public String getMahalle() {
+        return mahalle;
+    }
+
+    public void setMahalle(String mahalle) {
+        this.mahalle = mahalle;
+    }
+
+    public String getSokak() {
+        return sokak;
+    }
+
+    public void setSokak(String sokak) {
+        this.sokak = sokak;
+    }
+
 }
--- src/java/dao/MuracaatDAO.java
+++ src/java/dao/MuracaatDAO.java
@@ -4,105 +4,10 @@
  */
 package dao;
 
-import Entity.Muracaat;
-import static Various.ErrorFinder.DetectError;
-import java.sql.CallableStatement;
-import util.DBConnection;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author Eren
  */
-public class MuracaatDAO extends DBConnection {
-
-    private Connection db;
-    private Integer muracaat_bilgi_id;
-
-    private String mesaj;
-
-    public Integer MuracaatEkle(Muracaat muracaat, Integer KisiID) {
-        try {
-            Connection conn = this.getDb();
-
-            String callQueryMuracaatBilgi = "{call INSERT_MURACAAT_BILGI(?, ?, ?, ?, ?)}";
-            CallableStatement csMuracaatBilgi = conn.prepareCall(callQueryMuracaatBilgi);
-            csMuracaatBilgi.setInt(1, muracaat.getArsiv_dosya_no());
-            csMuracaatBilgi.setString(3, muracaat.getAciklama());
-            csMuracaatBilgi.setDate(4, new java.sql.Date(muracaat.getMuracaat_tarihi().getTime()));
-            csMuracaatBilgi.registerOutParameter(5, java.sql.Types.INTEGER);
-            csMuracaatBilgi.execute();
-            int muracaatBilgiId = csMuracaatBilgi.getInt(5);
-
-            this.mesaj = "İşlemler başarıyla gerçekleşmiştir.";
-
-        } catch (SQLException ex) {
-            DetectError(ex);
-        }
-    }
-
-    public List<Muracaat> GetList() {
-
-        List<Muracaat> UserList = new ArrayList<>();
-
-        try {
-
-            Statement statement = getDb().createStatement();
-
-            String Selectquery = "SELECT \n"
-                    + "JOIN KULLANICI_DURUM D ON K.kullanici_durum_id = D.kullanici_durum_id\n"
-                    + "JOIN KULLANICI_UNVAN U ON K.kullanici_unvan_id = U.kullanici_unvan_id";
-
-            ResultSet rs = statement.executeQuery(Selectquery);
-
-            while (rs.next()) {
-
-            }
-
-        } catch (Exception ex) {
-            DetectError(ex);
-        }
-        return UserList;
-    }
-
-    public void Delete(int kullaniciId) {
-        String deleteQuery = "DELETE FROM MURACAAT WHERE kullanici_id = " + kullaniciId;
-
-        try {
-            Statement statement = getDb().createStatement();
-
-            int rowsDeleted = statement.executeUpdate(deleteQuery);
-            System.out.println(rowsDeleted + " kisi silindi.");
-
-            this.mesaj = "İşlemler başarıyla gerçekleşmiştir.";
-
-        } catch (SQLException ex) {
-            DetectError(ex);
-        }
-    }
-
-    public Connection getDb() {
-        if (this.db == null) {
-            this.db = this.connect();
-        }
-        return db;
-    }
-
-    public void setDb(Connection db) {
-        this.db = db;
-    }
-
-    public Integer getMuracaat_bilgi_id() {
-        return muracaat_bilgi_id;
-    }
-
-    public void setMuracaat_bilgi_id(Integer muracaat_bilgi_id) {
-        this.muracaat_bilgi_id = muracaat_bilgi_id;
-    }
-
+public class MuracaatDAO {
+    
 }
