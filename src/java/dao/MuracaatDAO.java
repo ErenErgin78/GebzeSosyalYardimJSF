56609a5b3f286503fed03fefa794bdf2fa2ddd22
Administrator <Administrator@N57513>
2024-08-15 14:30:45 +0300

    muracaat

--- src/java/dao/MuracaatDAO.java
+++ src/java/dao/MuracaatDAO.java
@@ -99,7 +99,7 @@ public class MuracaatDAO extends DBConnection {
                         rs.getDate("KAYIT_TARIHI"),
                         rs.getInt("AKTIF"),
                         rs.getDate("GUNCELLEME_TARIHI"),
-                        rs.getBigDecimal("KIMLIK_NO").toBigInteger(),
+                        rs.getInt("KIMLIK_NO"),
                         rs.getString("ISIM"),
                         rs.getString("SOYISIM"),
                         rs.getInt("ARSIV_DOSYA_NO"),
