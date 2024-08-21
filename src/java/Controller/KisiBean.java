6dcbeb23355bf57cf58c553120b44fcb8a5eb258
aydiremm <ayd.iremm@gmail.com>
2024-08-21 14:59:07 +0300

    sorgulama denemesi

--- src/java/Controller/KisiBean.java
+++ src/java/Controller/KisiBean.java
@@ -43,7 +43,28 @@ public class KisiBean implements Serializable {
         this.entity = kisi;
     }
     
-
+    public Kisi KisiBul() {
+        if (entity.getKimlik_no() == null) {
+            return null;
+        }
+        Kisi bulunanKisi = dao.KisiBul(entity.getKimlik_no());        
+        
+        entity.setIsim(bulunanKisi.getIsim());
+        entity.setSoyisim(bulunanKisi.getSoyisim());
+        entity.setCinsiyet(bulunanKisi.getCinsiyet());
+        entity.setCilt_no(bulunanKisi.getCilt_no());
+        entity.setAile_sira_no(bulunanKisi.getAile_sira_no());
+        entity.setSira_no(bulunanKisi.getSira_no());
+        entity.setDogum_tarihi(entity.getDogum_tarihi());
+        entity.setMedeni_durum_id(entity.getMedeni_durum_id());
+        entity.setMedeni_durum_isim(entity.getMedeni_durum_isim());
+        entity.setKayit_tarihi(entity.getKayit_tarihi());
+        entity.setAktif(entity.getAktif());
+        
+        
+        
+        return bulunanKisi;
+    }
 
     public Kisi getEntity() {
         if (this.entity == null) {
