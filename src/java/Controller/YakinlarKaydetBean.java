728969a12444a8f421fa95060fd214427bed46a1
Administrator <Administrator@N57513>
2024-08-23 15:45:48 +0300

    hyhyhh

--- src/java/Controller/YakinlarKaydetBean.java
+++ src/java/Controller/YakinlarKaydetBean.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+* Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
 package Controller;
 
@@ -34,13 +34,98 @@ public class YakinlarKaydetBean implements Serializable {
 
     @Inject
     private EgitimDurumBean egitimDurumBean;
-    
+
     @Inject
     private TutanakSosyalGuvenceBean tutanakSosyalGuvenceBean;
-    
+
     @Inject
     private KisiEngelBean kisiEngelBean;
-    
+
     @Inject
     private MeslekBean meslekBean;
+
+    public void YakinlarEkle() {
+
+        try {
+
+            Integer kisi_id = kisiBean.ekle();
+
+            Integer yakinlar_id = kisiYakinlarBean.ekle();
+
+            Integer kisi_detay_id = kisiDetayBean.ekle();
+
+            
+            mesaj = "İşlem Başarılı";
+            
+        } catch (Exception ex) {
+            mesaj = DetectError(ex);
+
+        }
+
+    }
+
+    public String getMesaj() {
+        return mesaj;
+    }
+
+    public void setMesaj(String mesaj) {
+        this.mesaj = mesaj;
+    }
+
+    public KisiBean getKisiBean() {
+        return kisiBean;
+    }
+
+    public void setKisiBean(KisiBean kisiBean) {
+        this.kisiBean = kisiBean;
+    }
+
+    public KisiYakinlarBean getKisiYakinlarBean() {
+        return kisiYakinlarBean;
+    }
+
+    public void setKisiYakinlarBean(KisiYakinlarBean kisiYakinlarBean) {
+        this.kisiYakinlarBean = kisiYakinlarBean;
+    }
+
+    public KisiDetayBean getKisiDetayBean() {
+        return kisiDetayBean;
+    }
+
+    public void setKisiDetayBean(KisiDetayBean kisiDetayBean) {
+        this.kisiDetayBean = kisiDetayBean;
+    }
+
+    public EgitimDurumBean getEgitimDurumBean() {
+        return egitimDurumBean;
+    }
+
+    public void setEgitimDurumBean(EgitimDurumBean egitimDurumBean) {
+        this.egitimDurumBean = egitimDurumBean;
+    }
+
+    public TutanakSosyalGuvenceBean getTutanakSosyalGuvenceBean() {
+        return tutanakSosyalGuvenceBean;
+    }
+
+    public void setTutanakSosyalGuvenceBean(TutanakSosyalGuvenceBean tutanakSosyalGuvenceBean) {
+        this.tutanakSosyalGuvenceBean = tutanakSosyalGuvenceBean;
+    }
+
+    public KisiEngelBean getKisiEngelBean() {
+        return kisiEngelBean;
+    }
+
+    public void setKisiEngelBean(KisiEngelBean kisiEngelBean) {
+        this.kisiEngelBean = kisiEngelBean;
+    }
+
+    public MeslekBean getMeslekBean() {
+        return meslekBean;
+    }
+
+    public void setMeslekBean(MeslekBean meslekBean) {
+        this.meslekBean = meslekBean;
+    }
+    
 }
